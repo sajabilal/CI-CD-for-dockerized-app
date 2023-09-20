c44d144a2e3b1deddc7abe8f490dc7becdfb1263
Saja Bilal <102521358+sajabilal@users.noreply.github.com>
2023-09-20 20:51:22 +0200

    Delete routes directory

--- routes/route.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const trimVideo = require("../controllers/trimVideo");
-
-const route = express.Router();
-
-route.post('/trimVideo',trimVideo);
-
-module.exports = route;
